8fbcda4ef78ca95d281a19049ccbe9178491cd4d
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-07-28 15:53:48 +0700

    modifed sale

--- src/main/java/ru/netology/stats/Sale.java
+++ src/main/java/ru/netology/stats/Sale.java
@@ -1,7 +1,7 @@
 package ru.netology.stats;
 
 public class Sale {
-    public static void sale(long[] mountSale) {
+    public static void sale(long[] sales) {
         MinSales service = new MinSales();
         MaxSales service1 = new MaxSales();
         AvgSales service2 = new AvgSales();
