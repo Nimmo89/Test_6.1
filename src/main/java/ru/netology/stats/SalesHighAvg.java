e2ff0680ec89659c6a805fab623abc345a15671e
Nimmo89 <85748081+Nimmo89@users.noreply.github.com>
2021-07-28 15:46:41 +0700

    Add SalesHighAvg

--- src/main/java/ru/netology/stats/SalesHighAvg.java
+++ src/main/java/ru/netology/stats/SalesHighAvg.java
@@ -1,4 +1,15 @@
 package ru.netology.stats;
 
 public class SalesHighAvg {
+    public int salHighAvg(long[] sales) {
+        AvgSales service = new AvgSales();
+        int avgSales = service.avgSales(sales);
+        for (long saleMon : sales) {
+            if (saleMon > avgSales) {
+                System.out.println(saleMon);
+            }
+        }
+        return 0;
+    }
 }
+
---
+++ src/test/java/ru/netology/stats/SalesHighAvgTest.java
@@ -0,0 +1,12 @@
+package ru.netology.stats;
+import org.junit.jupiter.api.Test;
+
+public class SalesHighAvgTest {
+    @Test
+    public void LowAvg() {
+        SalesHighAvg service = new SalesHighAvg();
+        long[] sales = {8,15,13,15,17,20,19,20,7,14,14,18};
+        long LowAvg = service.salHighAvg(sales);
+        System.out.println(LowAvg);
+    }
+}
